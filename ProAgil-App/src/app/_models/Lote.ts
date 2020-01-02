b226e06584c0e4c2fbf589fc459b2ba393a83e4c
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2020-01-02 16:33:26 -0300

    Angular - Organizando Projeto

---
+++ ProAgil-App/src/app/_models/Lote.ts
@@ -0,0 +1,9 @@
+export interface Lote {
+    id: number;
+    nome: string;
+    preco: number;
+    dataInicio?: Date;
+    dataFim?: Date;
+    qtd: number;
+    eventoId: number;
+}
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-eventos',
@@ -8,7 +11,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+  eventos: Evento[];
+  eventosFiltrados: Evento[];
+  imagemLargura = 100;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  modalRef: BsModalRef;
+
   filtro: string;
+
+  constructor(
+      private eventoService: EventoService
+    , private modalService: BsModalService
+  ) { }
+
   get filtroLista(): string {
     return this.filtro;
   }
@@ -17,19 +33,15 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  eventos: any = [];
-  eventosFiltrados: any = [];
-  imagemLargura = 100;
-  imagemMargem = 2;
-  mostrarImagem = false;
-
-  constructor(private http: HttpClient) { }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  filtrarEventos(filtrarPor: string): any {
+  filtrarEventos(filtrarPor: string): Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
@@ -37,14 +49,15 @@ export class EventosComponent implements OnInit {
   }
 
   alternarImagem() {
-    this.mostrarImagem = !this.mostrarImagem;
+    this.mostrarImagem = !this.mostrarImagem; 
   }
 
   getEventos() {
-    this.http.get('http://localhost:5000/api/Eventos').subscribe(response => {
-      this.eventos = response;
-      this.eventosFiltrados = response;
-      console.log(this.eventos);
+    this.eventoService.getAllEvento().subscribe(
+      (_eventos: Evento[]) => {
+      this.eventos = _eventos;
+      this.eventosFiltrados = this.eventos;
+      console.log(_eventos);
     }, error => {
       console.log(error);
     });
